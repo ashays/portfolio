560d7ea5c9ed8741ccd846777ea2b3c59e2e64f3
Ashay Sheth <ashay@ashays-air.lan>
2020-04-26 18:13:14 -0400

    Updates

---
+++ precache-manifest.6957901171a24eaf020aed5649f9603b.js
@@ -0,0 +1,74 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "71b216144c7f1c4a08b7c8086491066e",
+    "url": "/index.html"
+  },
+  {
+    "revision": "d54798675a38b3e6c789",
+    "url": "/static/css/main.2e17a535.chunk.css"
+  },
+  {
+    "revision": "79d8a25d7dd2c7663ac6",
+    "url": "/static/js/2.d011e0d2.chunk.js"
+  },
+  {
+    "revision": "c8ffd5abc06d09fa1ebb018a30d7c2c6",
+    "url": "/static/js/2.d011e0d2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d54798675a38b3e6c789",
+    "url": "/static/js/main.106b09e3.chunk.js"
+  },
+  {
+    "revision": "ac64930fece451d0d2ee",
+    "url": "/static/js/runtime-main.394cc986.js"
+  },
+  {
+    "revision": "17f0c53e0252fe90ba8531df2f7dc96b",
+    "url": "/static/media/raleway-bold-webfont.17f0c53e.svg"
+  },
+  {
+    "revision": "7027f9c418dbd2d2e653f134764bed81",
+    "url": "/static/media/raleway-bold-webfont.7027f9c4.eot"
+  },
+  {
+    "revision": "8f2eb8e2028c60c9b18469465fbfb7ab",
+    "url": "/static/media/raleway-bold-webfont.8f2eb8e2.ttf"
+  },
+  {
+    "revision": "a8822eae8f9fa55f6d64a41a68414386",
+    "url": "/static/media/raleway-bold-webfont.a8822eae.woff"
+  },
+  {
+    "revision": "3191a00a80556475983ca378acb4aae1",
+    "url": "/static/media/raleway-light-webfont.3191a00a.eot"
+  },
+  {
+    "revision": "c0a42fefec8e1c4cbf5dd034892c825f",
+    "url": "/static/media/raleway-light-webfont.c0a42fef.svg"
+  },
+  {
+    "revision": "fc899587cfe7a567aaf811f294445460",
+    "url": "/static/media/raleway-light-webfont.fc899587.woff"
+  },
+  {
+    "revision": "ff0d18614e094801fe20d9ed0ca6756e",
+    "url": "/static/media/raleway-light-webfont.ff0d1861.ttf"
+  },
+  {
+    "revision": "3191943eda84d53431a6526c187af3b6",
+    "url": "/static/media/raleway-regular-webfont.3191943e.svg"
+  },
+  {
+    "revision": "85183718cbf6ccdbd699fb7fe9054d9d",
+    "url": "/static/media/raleway-regular-webfont.85183718.ttf"
+  },
+  {
+    "revision": "ac3c42687830741c46f6dd85b3e170b7",
+    "url": "/static/media/raleway-regular-webfont.ac3c4268.eot"
+  },
+  {
+    "revision": "ec223b9a769d928239aa6d900cb314ef",
+    "url": "/static/media/raleway-regular-webfont.ec223b9a.woff"
+  }
+]);
