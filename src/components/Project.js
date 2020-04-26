02d2f66b0e9d16f814bce5a74a8c58d000fca21e
Ashay Sheth <ashay@ashays-air.lan>
2020-04-26 18:06:22 -0400

    Escape key to exit project view

--- src/components/Project.js
+++ src/components/Project.js
@@ -13,21 +13,32 @@ class Project extends Component {
         this.project = projects[i];
       }
     }
+    this.escape = this.escape.bind(this);
   }
 
   componentDidMount() {
     document.title = this.project.name + " / Ashay Sheth";
     document.body.style.backgroundColor = this.project.color;
+    document.addEventListener("keyup", this.escape);
     window.scrollTo(0, 0);
+  }
+
+  componentWillUnmount() {
+    document.removeEventListener("keyup", this.escape)
+  }
+
+  escape(e) {
+    if (e.code === 'Escape') {
+      this.props.history.push("/");
+    }
   }
 
   render() {
     var assetBlock = false;
     if (this.project.graphics) {
-      assetBlock = [];
-      this.project.graphics.map(function(graphic, i) {
-        assetBlock.push(<img key={i} width={graphic.width} height={graphic.height} src={graphic.source} alt={graphic.description} />);
-      });
+      assetBlock = this.project.graphics.map((graphic, i) => 
+        (<img key={i} width={graphic.width} height={graphic.height} src={graphic.source} alt={graphic.description} />)
+      );
     }
     return (
       <div>
